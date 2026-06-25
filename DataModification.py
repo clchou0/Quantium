1d54baf3e4f5d7076c222d7c67d1703775194ec8
Charlie Chou <changlin.chou@student.uts.edu.au>
2026-06-25 15:42:00 +1000

    task 2

---
+++ DataModification.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import glob as glob
+
+files = glob.glob('data/*.csv')
+df = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
+
+df = df[df['product'] == 'pink morsel']
+df['price'] = df['price'].str.replace('$', '', regex=False).astype(float)
+df['sales'] = df['price'] * df['quantity']
+
+result = df[['sales', 'date', 'region']]
+
+result.to_csv('output.csv', index=False)
